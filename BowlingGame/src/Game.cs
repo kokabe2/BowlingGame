c0fd704f30111b490933d25009ab0c35b3be45a7
Ken Okabe <ken.okabe.1.168@gmail.com>
2019-06-08 17:14:51 +0900

    Add Game class

---
+++ BowlingGame/src/Game.cs
@@ -0,0 +1,20 @@
+﻿// Copyright(c) 2019 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+using System;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        public int Score
+        {
+            get;
+            private set;
+        } = 0;
+
+        public void Add(int pins)
+        {
+            Score += pins;
+        }
+    }
+}
---
+++ BowlingGame/test/GameTest.cs
@@ -0,0 +1,21 @@
+﻿// Copyright(c) 2019 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+using System;
+using Xunit;
+using BowlingGame;
+
+namespace BowlingGameTest
+{
+    public class GameTest
+    {
+        [Fact]
+        public void TestOneThrow()
+        {
+            Game g = new Game();
+
+            g.Add(5);
+
+            Assert.Equal(5, g.Score);
+        }
+    }
+}
