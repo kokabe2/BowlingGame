ac989ec676909529922aa9b0089c678abcd078ab
Ken Okabe <ken.okabe.1.168@gmail.com>
2019-06-08 17:06:23 +0900

    Add Frame class

---
+++ BowlingGame/src/Frame.cs
@@ -0,0 +1,21 @@
+﻿// Copyright(c) 2019 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+using System;
+
+namespace BowlingGame
+{
+    public class Frame
+    {
+        int itsScore = 0;
+
+        public int GetScore()
+        {
+            return itsScore;
+        }
+
+        public void Add(int pins)
+        {
+            itsScore += pins;
+        }
+    }
+}
---
+++ BowlingGame/test/FrameTest.cs
@@ -0,0 +1,29 @@
+﻿// Copyright(c) 2019 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+using System;
+using Xunit;
+using BowlingGame;
+
+namespace BowlingGameTest
+{
+    public class FrameTest
+    {
+        [Fact]
+        public void TestScoreNoThrows()
+        {
+            Frame f = new Frame();
+
+            Assert.Equal(0, f.GetScore());
+        }
+
+        [Fact]
+        public void TestAddOneThrow()
+        {
+            Frame f = new Frame();
+
+            f.Add(5);
+
+            Assert.Equal(5, f.GetScore());
+        }
+    }
+}
