a5010da8516a9c120fb85b82c1d0ba4831a263b8
Ken Okabe <ken.okabe.1.168@gmail.com>
2019-06-08 16:56:42 +0900

    Initial commit

---
+++ BowlingGame/test/ThrowTest.cs
@@ -0,0 +1,15 @@
+// Copyright(c) 2019 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+using System;
+using Xunit;
+
+namespace BowlingGameTest
+{
+    public class ThrowTest
+    {
+        [Fact]
+        public void TestSomething()
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Bowling Game Case Study
+
+C# implementation of Bowling Game Case Study from the book "Agile software development : principles, patterns, and practices." by Robert C. Martin.
